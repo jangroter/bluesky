2c26ce049d2053c6976d68b03a631f24e3828f5d
jangroter <47716241+jangroter@users.noreply.github.com>
2022-08-04 15:13:30 +0200

    added fuel consumption functionalities

--- plugins/fuelconsumption.py
+++ plugins/fuelconsumption.py
@@ -5,11 +5,13 @@ Plugin to keep track of all the fuel used by the different aircraft according to
 import numpy as np
 import bluesky as bs
 from bluesky import core, stack, traf
+from openap import FuelFlow
 
-
-timestep = 1
+fuelconsumption = None
 
 def init_plugin():
+
+    global fuelconsumption
     fuelconsumption = Fuelconsumption()
 
     config = {
@@ -23,16 +25,21 @@ class Fuelconsumption(core.Entity):
     def __init__(self):
         super().__init__()
         
+        self.ff = FuelFlow('a320')
+        self.mass = 66000
+
         with self.settrafarrays():
             self.fuelconsumed = np.array([])
+            self.fuelconsumedtotal = np.array([])
         
     def create(self, n=1):
         super().create(n)
-        self.fuelconsumed[-n:] = [0] * n
+        self.fuelconsumed[-n:] = 0
+        self.fuelconsumedtotal[-n:] = 0
 
-    @core.timed_function(name='fuelconsumption', dt = timestep)
-    def update(self):
-        self.fuelconsumed += traf.perf.fuelflow * timestep
-        for acid in traf.id:
-            idx = traf.id2idx(acid)
-            print(f'AC {acid}, fuel consumed: {self.fuelconsumed[idx]} [kg]')
+    def update(self, dt):
+        for i in range(traf.ntraf):
+            path_angle = np.degrees(np.arctan2(traf.vs[i],traf.tas[i]))
+            fuelflow = self.ff.enroute(self.mass,traf.tas[i],traf.alt[i],path_angle)
+            self.fuelconsumed[i] += fuelflow*dt
+            self.fuelconsumedtotal[i] += fuelflow*dt
